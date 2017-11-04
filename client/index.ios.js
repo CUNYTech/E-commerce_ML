ca4c97dd28056ee7fd6687733e2b259e5319118f
Brian <blykes@gmail.com>
2017-11-04 01:25:26 -0400

    began firebase implimentation

--- client/index.ios.js
+++ client/index.ios.js
@@ -3,11 +3,12 @@ import { AppRegistry
  } from 'react-native';
  import { StackNavigator } from 'react-navigation';
 
+
 import Splash from './Splash';
 import Login from './src/components/Login/Login';
 import Register from './src/components/Register/Register';
 import Main from './src/components/Main';
-
+import * as firebase from 'firebase';
 export const App = StackNavigator({
 
   Login: { screen: Login },
@@ -16,6 +17,16 @@ export const App = StackNavigator({
 
 });
 
+//initialize firebase 
+const firebaseConfig = {
+apikey:  "<our api key goes here>",
+authDomian: "<our auth domain>",
+databaseURL: "<our database url>",
+storageBucket: "<our storage bucket goes here>",
+};
+const firebaseApp.initialize(firebaseConfig);
+//end method
+ 
 export default class AwesomeProject extends Component {
   render() {
     return (
